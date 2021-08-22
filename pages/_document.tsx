20f4fe30bd6ba27330a75be6007fa87347accd01
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-08-22 17:11:47 -0600

    feat: add `Inter` font family to Next.js `_document`

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
