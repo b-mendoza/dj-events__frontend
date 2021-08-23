e6f81ed610c0109d2b737f44145b7c7a9bb0ae31
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-08-22 21:06:23 -0600

    feat: add `constants` directory

---
+++ constants/shared.ts
@@ -0,0 +1,13 @@
+export const cssVariables = {
+  colors: {
+    primaryColor: '#333',
+    primaryLightBlueColor: '#20a4f3',
+    primaryRedColor: '#f44336',
+    primaryWhiteColor: '#fff',
+  },
+  breakpoints: {
+    mobileBreakpoint: '340px',
+    tabletBreakpoint: '768px',
+    desktopBreakpoint: '1200px',
+  },
+};
