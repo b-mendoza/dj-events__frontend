c1993306e8c722dee06162eda84319ea0e0d1ffa
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-08-22 23:06:47 -0600

    feat: add `Image` component to `Home` page component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
+
 import { MainLayout } from 'components';
+
+import styles from 'styles/pages/Home.module.scss';
 
 function Home() {
   return (
     <MainLayout>
-      <h1>Home Page</h1>
+      <Image
+        alt="Hero Image"
+        className={styles['hero']}
+        height="570px"
+        layout="responsive"
+        priority
+        src="/hero.jpg"
+        width="1200px"
+      />
     </MainLayout>
   );
 }
