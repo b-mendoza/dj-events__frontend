5c8e8a78dd7b79250e156eee1e1d5534215d9eb4
Bryan Mendoza <bmendoza.dev@hotmail.com>
2021-08-22 21:07:29 -0600

    feat: add SSR support for Material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import 'styles/main.scss';
 
 import { AppProps } from 'next/app';
+import { useEffect } from 'react';
 
 function __NextApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+
+    if (jssStyles) jssStyles.parentElement?.removeChild(jssStyles);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,43 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from 'next/document';
+import { Children } from 'react';
 
 class __NextDocument extends Document {
+  static async getInitialProps(context: DocumentContext) {
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = context.renderPage;
+
+    context.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: App => props => sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(context);
+
+    return {
+      ...initialProps,
+      styles: [
+        ...Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link href="https://fonts.gstatic.com" rel="preconnect" />
+          <link
+            crossOrigin="anonymous"
+            href="https://fonts.gstatic.com"
+            rel="preconnect"
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
             rel="stylesheet"
